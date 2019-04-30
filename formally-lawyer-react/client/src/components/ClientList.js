a8e2cfb7c06a7441fc98b16a8172042fd38467e7
Ben Deckey <BDeckey@bens-mbp-5.devices.brown.edu>
2019-04-30 19:06:04 -0400

    adding real data to client list

--- formally-lawyer-react/client/src/components/ClientList.js
+++ formally-lawyer-react/client/src/components/ClientList.js
@@ -2,25 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import { fade } from '@material-ui/core/styles/colorManipulator';
 import SearchIcon from '@material-ui/icons/Search';
 import InputBase from '@material-ui/core/InputBase';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Collapse from '@material-ui/core/Collapse';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import SendIcon from '@material-ui/icons/Send';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
-import StarBorder from '@material-ui/icons/StarBorder';
 import AssignmentIcon from '@material-ui/icons/Assignment';
-import Avatar from '@material-ui/core/Avatar';
-import ClientPage from './ClientPage';
 import { NavLink } from 'react-router-dom';
 
 const styles = theme => ({
